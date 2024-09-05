c200d57eb7f0a4e9aee8141b430d7d5baabc78c5
Jean Carlos <je.franco@duocuc.cl>
2024-09-04 23:05:35 -0400

    se agrega vista historial de asistencia

--- TarnishedTracker/src/app/components/components.module.ts
+++ TarnishedTracker/src/app/components/components.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { HeaderComponent } from './header/header.component';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatDividerModule} from '@angular/material/divider';
 
 
 
